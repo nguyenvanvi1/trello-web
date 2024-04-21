1960ae27abcfbdb32811ddbf49560d72b426acff
nguyenvanvi1 <nvvi.nvla1.2019@gmail.com>
2024-04-22 00:49:19 +0700

    board content

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -4,29 +4,29 @@ import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
 import { useColorScheme } from '@mui/material'
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
   const handleChange = (event) => {
-        const selectedMode = event.target.value
-        setMode(selectedMode)
-  };
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
   return (
-    <FormControl size='small' sx={{minWidth:'120px'}}>
-      <InputLabel 
-      sx={{
-        color:'white',
-        '&.Mui-focused':{color:'white'}
-      }}
-      id="label-select-dark-light-mode">Mode</InputLabel>
+    <FormControl size='small' sx={{ minWidth:'120px' }}>
+      <InputLabel
+        sx={{
+          color:'white',
+          '&.Mui-focused':{ color:'white' }
+        }}
+        id="label-select-dark-light-mode">Mode</InputLabel>
       <Select
         sx={{
           color:'white',
-          '.MuiOutlinedInput-notchedOutline':{borderColor:'white'},
-          '&:hover .MuiOutlinedInput-notchedOutline':{borderColor:'white'},
-          '.MuiSvgIcon-root':{color:'white'}
+          '.MuiOutlinedInput-notchedOutline':{ borderColor:'white' },
+          '&:hover .MuiOutlinedInput-notchedOutline':{ borderColor:'white' },
+          '.MuiSvgIcon-root':{ color:'white' }
         }}
         labelId="label-select-dark-light-mode"
         id="select-dark-light-mode"
@@ -35,19 +35,19 @@ function ModeSelect() {
         onChange={handleChange}
       >
         <MenuItem value='light'>
-            <div style={{ display:'flex', alignItems:'center',gap:'8px' }}>
+          <div style={{ display:'flex', alignItems:'center', gap:'8px' }}>
             <LightModeIcon fontSize='small'/>Light
-            </div>
+          </div>
         </MenuItem>
         <MenuItem value='dark'>
-            <div style={{ display:'flex', alignItems:'center',gap:'8px' }}>
+          <div style={{ display:'flex', alignItems:'center', gap:'8px' }}>
             <DarkModeIcon fontSize='small'/>Dark
-            </div>
+          </div>
         </MenuItem>
-        <MenuItem value='system'>           
-            <div style={{ display:'flex', alignItems:'center',gap:'8px' }}>
+        <MenuItem value='system'>
+          <div style={{ display:'flex', alignItems:'center', gap:'8px' }}>
             <SettingsBrightnessIcon fontSize='small'/>  System
-            </div>
+          </div>
         </MenuItem>
       </Select>
     </FormControl>
