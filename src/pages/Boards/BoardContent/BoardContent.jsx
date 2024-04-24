c65471508e99b48a336687ede2032fce4289e7f1
nguyenvanvi1 <nvvi.nvla1.2019@gmail.com>
2024-04-24 22:06:17 +0700

    overDragg

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -6,6 +6,7 @@ import { DndContext,PointerSensor,useSensor,useSensors,MouseSensor,TouchSensor,D
 import { arrayMove } from '@dnd-kit/sortable'
 import Column from './ListColumns/Column/Column'
 import TrelloCard from './ListColumns/Column/ListCards/Card/TrelloCard'
+import { cloneDeep } from 'lodash'
 const ACTIVE_DRAG_ITEM_TYPE = {
   COLUMN: 'ACTIVE_DRAG_ITEM_TYPE_COLUMN',
   CARD:'ACTIVE_DRAG_ITEM_TYPE_CARD'
@@ -23,13 +24,53 @@ function BoardContent({ board }) {
     const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
     setOrderedColumns(orderedColumns)
   }, [board])
+  const findColumnByCardId = (cardId)=>{
+    return orderedColumns.find(column=>column.cards.map(card=>card._id)?.includes(cardId))
+  }
   const handleDragStart = (event)=>{
     setActiveDragItemId(event?.active?.id)
     setActiveDragItemType(event?.active?.data?.current?.columnId ? ACTIVE_DRAG_ITEM_TYPE.CARD : ACTIVE_DRAG_ITEM_TYPE.COLUMN)
     setActiveDragItemData(event?.active?.data?.current)
   }
+  const handleDragOver = (event)=>{
+    const {active,over} = event
+    if(!over) return
+    const {id:activeDraggingCardId, data:{current:activeDraggingCardData}} = active
+    const {id:overCardId} = over
+    const activeColumn =findColumnByCardId(activeDragItemId)
+    const overColumn = findColumnByCardId(overCardId)
+    if(!activeColumn || !overColumn) return 
+    if(activeColumn._id !== overColumn._id) {
+      setOrderedColumns(prevColumns=>{
+        const overCardIndex = overColumn?.cards?.findIndex(card=>card._id ===overCardId)
+        let newCardIndex
+        const isBelowOverItem =
+        active.rect.current.translated &&
+        active.rect.current.translated.top > over.rect.top +over.rect.height
+        const modifier = isBelowOverItem ? 1:0
+        newCardIndex =overCardIndex >= 0 ? overCardIndex + modifier : overColumn?.cards?.length + 1
+        const nextColumns =cloneDeep(prevColumns)
+        const nextActiveColumn = nextColumns.find(column=>column._id === activeColumn._id)
+        const nextOverColumn = nextColumns.find(column=>column._id === overColumn._id)
+        if(nextActiveColumn) {
+          nextActiveColumn.cards = nextActiveColumn.cards.filter(card=>card._id !== activeDraggingCardId)
+          nextActiveColumn.cardOrderIds = nextActiveColumn.cards.map(car=>car._id)
+        }
+        if(nextOverColumn) {
+          nextOverColumn.cards = nextOverColumn.cards.filter(card=>card._id !== activeDraggingCardId)
+          nextOverColumn.cards = nextOverColumn.cards.toSpliced(newCardIndex,0,activeDraggingCardData)
+          nextOverColumn.cardOrderIds = nextOverColumn.cards.map(car=>car._id)
+        }
+        return nextColumns
+      })
+      console.log('hihi')
+    }
+  }
   const handleDragEnd = (event) => {
     console.log('hello', event)
+    if (activeDragItemType ==ACTIVE_DRAG_ITEM_TYPE.CARD) {
+      return
+    }
     const { active, over } = event
     if (!over) return
     if (active.id !== over.id) {
@@ -54,7 +95,7 @@ function BoardContent({ board }) {
   }
   return (
 
-    <DndContext onDragEnd={handleDragEnd} sensors={sensors} onDragStart={handleDragStart}>
+    <DndContext onDragEnd={handleDragEnd} onDragOver={handleDragOver} sensors={sensors} onDragStart={handleDragStart}>
       <Box sx={{
         bgcolor:(theme ) => (theme.palette.mode ==='dark'?'#34495e':'#1976d2'),
         width:'100%',
--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/TrelloCard.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/TrelloCard.jsx
@@ -21,7 +21,8 @@ function TrelloCard({card}) {
     touchAction:'none',
     transform: CSS.Translate.toString(transform),
     transition,
-    opacity:isDragging ? 0.5 :undefined
+    opacity:isDragging ? 0.5 :undefined,
+    border:isDragging ? '1px solid #2ecc71' :undefined
   }
   const shouldShowCardActions = ()=>{
     return !!card?.memberIds?.length || !!card?.memberIds?.length || !!card?.memberIds?.length
