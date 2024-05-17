9a7fc9b2b07c63fc2e8dd99c8398ad216082e415
nguyenvanvi1 <nvvi.nvla1.2019@gmail.com>
2024-05-17 16:08:14 +0700

    add axios

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, Container} from '@mui/material'
 import BoardContent from './BoardContent/BoardContent'
 import BoardBar from './BoardBar/BoardBar'
 import AppBar from '~/components/AppBar/AppBar'
 import { mockData } from '~/apis/mock-data'
+import { fetchBoardDetailsAPI } from '~/apis'
 function Board() {
+  const [board,setBoard] = useState(null)
+  useEffect(()=>{
+    const boardId = '6644ef638e67c6e75a85d08e'
+    fetchBoardDetailsAPI(boardId).then(board=>{
+      setBoard(board)
+    })
+  },[])
   return (
     <>
       <Container disableGutters maxWidth={false} sx={{ height:'100vh', backgroundColor:'primary.main' }}>
         <AppBar/>
-        <BoardBar board = {mockData?.board} />
-        <BoardContent board = {mockData?.board}/>
+        <BoardBar board = {board} />
+        <BoardContent board = {board}/>
       </Container>
     </>
   )
