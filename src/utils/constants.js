77ad51016efa77fe362bf0b11a6d1104ade3cffc
nguyenvanvi1 <nvvi.nvla1.2019@gmail.com>
2024-06-01 18:12:27 +0700

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,8 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+if(process.env.BUILD_MODE ==='dev') {
+   apiRoot = 'http://localhost:8017' 
+}
+if(process.env.BUILD_MODE =='production') {
+    apiRoot = 'https://trello-api-t8c5.onrender.com'
+}
+export const API_ROOT = apiRoot
